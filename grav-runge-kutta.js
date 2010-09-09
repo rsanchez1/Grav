0382cac70f9024f21d0e2267eaf57157826e7e40
Roberto Sanchez <rsanchez1990@gmail.com>
2010-09-09 12:34:44 -0400

    more experiments with symplectic integrator

--- grav-runge-kutta.js
+++ grav-runge-kutta.js
@@ -263,7 +263,7 @@ function symplectic(state, derivative, c, d, getEnergy, colliders1, colliders2) 
         if (!state[i].momentum) {
             momentum = state[i].velocity;
         } else {
-            momentum = state[i].momentum;
+            momentum = state[i].momentum.multiply(1/bodies[i].mass);
         }
         derivative[i].position = state[i].position.add(momentum.multiply(c));
     }
@@ -299,16 +299,16 @@ function symplectic(state, derivative, c, d, getEnergy, colliders1, colliders2) 
             if (!state[i].momentum) {
                 momentumi = state[i].velocity;
             } else {
-                momentumi = state[i].momentum;
+                momentumi = state[i].momentum.multiply(1/bodies[i].mass);
             }
             var momentumj = [];
             if (!state[j].momentum) {
                 momentumj = state[j].velocity;
             } else {
-                momentumj = state[j].momentum;
+                momentumj = state[j].momentum.multiply(1/bodies[j].mass);
             }
-            derivative[i].momentum = momentumi.subtract(diff.multiply(multi * d));
-            derivative[j].momentum = momentumj.subtract(diff.multiply(multj * d));
+            derivative[i].momentum = derivative[i].momentum.add(momentumi.subtract(diff.multiply(multi * d)));
+            derivative[j].momentum = derivative[j].momentum.add(momentumj.subtract(diff.multiply(multj * d)));
             if (getEnergy) {
                 totalEnergy -= (gravConstant * bodies[j].mass * bodies[i].mass) / state[i].position.distanceFrom(state[j].position);
             }
@@ -523,8 +523,8 @@ function calculateOrbit() {
         bodies[i].position = bodies[i].position.add((derivative1[i].position.add(derivative4[i].position).add(derivative3[i].position.multiply(2))).multiply(h6));
         bodies[i].velocity = bodies[i].velocity.add((derivative1[i].velocity.add(derivative4[i].velocity).add(derivative3[i].velocity.multiply(2))).multiply(h6));
         */
-        bodies[i].position = derivative3[i].position.add(derivative3[i].momentum.multiply(1/(2*(2-beta))));
-        bodies[i].velocity = derivative3[i].momentum;
+        bodies[i].position = derivative3[i].position.add(derivative3[i].momentum.multiply(1/(2*bodies[i].mass*(2-beta))));
+        bodies[i].velocity = derivative3[i].momentum.multiply(1/bodies[i].mass);
         energy += .5 * bodies[i].mass * bodies[i].velocity.dot(bodies[i].velocity);
         drawBody(bodies[i].position[0], bodies[i].position[1], bodies[i].radius, bodies[i].color, paper);
         if (alpha < 1) {
